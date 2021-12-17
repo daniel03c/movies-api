6e5e9620b2dfa65774a482d263e78bed4cc1d98b
Daniel Martinez <jdaniel_03c@hotmail.com>
2021-12-17 15:24:55 -0600

    cors

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ mongoose.connect(process.env.DB_URI,
             .then(() => console.log('dbcinema connection successful'))
             .catch((err) => console.error(err));
 
-app.use(cors());
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
